0c527ef17b2684c5575ef0712b76f0ff1b0a8de2
Riya <riyaa7vermaa@gmail.com>
2021-08-02 16:11:48 +0530

    added failed page

---
+++ Covid/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const api = require('novelcovid');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+api.settings({
+    baseUrl: 'https://disease.sh'
+})
+
+app.get("/", function (req, res) {
+    api.countries().then(function (response) {
+        res.render("home", { info: response });
+        console.log(response);
+    })
+
+})
+app.post("/",function(req,res){
+    let searched=req.body.val;
+  
+        api.countries({country:`${searched}`}).then(function(response){
+            console.log(response);
+        res.render("search",{info:response})
+
+    // }) ;
+    })
+
+})
+
+
+app.listen(process.env.PORT||3000, function () {
+    console.log("Server started on port 3000");
+});
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const api = require('novelcovid');
 
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,29 +12,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 api.settings({
-    baseUrl: 'https://disease.sh'
-})
+        baseUrl: 'https://disease.sh'
+    }) /
+    app.get("/", function(req, res) {
+        api.countries().then(function(response) {
+            res.render("home", { info: response });
+            // console.log(response);
+        })
 
-app.get("/", function (req, res) {
-    api.countries().then(function (response) {
-        res.render("home", { info: response });
+    })
+
+
+app.post("/", function(req, res) {
+
+    let searched = req.body.val;
+
+    api.countries({ country: `${searched}` }).then(function(response) {
         console.log(response);
-    })
+        let result = response.country;
+        if (result) {
+            res.render("search", { info: response })
+        } else {
+            res.render("failed");
 
-})
-app.post("/",function(req,res){
-    let searched=req.body.val;
-  
-        api.countries({country:`${searched}`}).then(function(response){
-            console.log(response);
-        res.render("search",{info:response})
-
-    // }) ;
+        }
     })
 
 })
 
 
-app.listen(process.env.PORT||3000, function () {
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server started on port 3000");
 });
